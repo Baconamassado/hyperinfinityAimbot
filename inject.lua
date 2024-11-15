e274806abac46cbe08199a5abe3835fb5ac09975
Bacon <149910312+Baconamassado@users.noreply.github.com>
2024-11-14 21:15:46 -0300

    Update inject.lua

--- inject.lua
+++ inject.lua
@@ -1,1 +1,21 @@
+local function sendnot(title, desc, duration, icon)
+    if icon == 'not' then 
+        Notification.new({
+            Title = title,
+            Description = desc,
+            Duration = duration,
+            Icon = "rbxassetid://8997385628"
+        })
+        print("Sended notification, title: ", title, "Desc: ", desc, "Duration: ", duration, "Icon: rbxassetid://8997385628")
+    else
+        Notification.new({
+            Title = title,
+            Description = desc,
+            Duration = duration,
+            Icon = icon
+        })
+        print("Sended notification, title: ", title, "Desc: ", desc, "Duration: ", duration, "Icon: ", icon)
+    end
+end
+
 sendnot("Update in progress", "This update is experimental, expect bugs", 2, "not")
