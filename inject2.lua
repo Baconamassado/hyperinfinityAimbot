0163b79051b0241cfbc60a247c6ad2dfbd39fbfd
Bacon <149910312+Baconamassado@users.noreply.github.com>
2024-11-15 22:35:09 -0300

    Update inject2.lua

--- inject2.lua
+++ inject2.lua
@@ -1,10 +1,6 @@
 local NothingLibrary = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/3345-c-a-t-s-u-s/NOTHING/main/source.lua'))()
 local Notification = NothingLibrary.Notification()
 
-if getgenv().owner ~= "Bacon" then
-    while true do end -- Crash skidding people LOL, It's kind of useless, just to troll
-end
-
 local function sendnot(title, desc, duration, icon)
     if icon == 'not' then 
         Notification.new({
